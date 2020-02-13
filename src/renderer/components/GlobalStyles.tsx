0b2557d3a51218618c29ef9d80672fb8cb4e1976
fred <jungdujang@gmail.com>
2020-02-13 22:41:50 +0900

    Fix: 오타 수정

--- src/renderer/components/GlobalStyles.tsx
+++ src/renderer/components/GlobalStyles.tsx
@@ -8,7 +8,7 @@ const globalStyles = createGlobalStyle`
          color:inherit;
      }
      *{
-         box-sizing:boerder-box;
+         box-sizing: border-box;
      }
      body{
          font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
