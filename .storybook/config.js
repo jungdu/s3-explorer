a8a8d0e5d69dabf640481a6cc2006f3a0398436b
fred <jungdujang@gmail.com>
2020-02-14 01:33:23 +0900

    Add: storybook에 global style 적용!

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { addDecorator, configure } from '@storybook/react';
+import GlobalStyles from "../src/renderer/components/GlobalStyles";
+
+addDecorator(s => <><GlobalStyles />{s()}</>);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/renderer', true, /\.stories\.(js|mdx|tsx)$/), module);
